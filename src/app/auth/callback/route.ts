3c6f8f0b4ad6671c8dc5a0e3a522ec3c2317d1ac
nasgunawann <nasgunawann@gmail.com>
2025-11-24 12:05:33 +0700

    fix login

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
 
   if (code) {
     const response = NextResponse.redirect(`${requestUrl.origin}${next}`);
-    
+
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
