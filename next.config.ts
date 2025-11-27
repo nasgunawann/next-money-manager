3e0eea4ba35f109700aadd1fd710ab836309f013
nasgunawann <nasgunawann@gmail.com>
2025-11-28 00:03:07 +0700

    commented out serwist to check if its the problemomasdioajdsoaid

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
-import withSerwistInit from "@serwist/next";
+// import withSerwistInit from "@serwist/next";
 
-const withSerwist = withSerwistInit({
-  swSrc: "src/sw.ts",
-  swDest: "public/sw.js",
-  disable: true, // Service worker disabled for testing
-  register: false,
-});
+// const withSerwist = withSerwistInit({
+//   swSrc: "src/sw.ts",
+//   swDest: "public/sw.js",
+//   disable: true, // Service worker disabled for testing
+//   register: false,
+// });
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig; // withSerwist(nextConfig);
