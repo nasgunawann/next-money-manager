a572230a2121e317f2d2ef04e6cc34c3bbe352ce
nasgunawann <nasgunawann@gmail.com>
2025-11-27 20:12:06 +0700

    Integrate Serwist for PWA

---
+++ src/sw.ts
@@ -0,0 +1,21 @@
+/// <reference lib="webworker" />
+
+import { defaultCache } from "@serwist/next/worker";
+import { Serwist } from "serwist";
+
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: Array<string | { url: string; revision?: string }>;
+};
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+serwist.addEventListeners();
+
+export {};
+
